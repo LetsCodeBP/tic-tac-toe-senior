84371b8a95a1de99bfc4e7834ad15453d7cfc95d
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2017-09-25 19:10:20 +0200

    refactor(render): separate nested loops to functions

--- render.js
+++ render.js
@@ -2,14 +2,18 @@
 
 export default function renderGame(gameRoot, gameState) {
   const mainTable = document.createElement('table')
-  gameState.forEach(row => {
-    const tableRow = document.createElement('tr')
-    row.forEach(cell => {
-      const tableCell = document.createElement('td')
-      tableCell.innerHTML = cell
-      tableRow.appendChild(tableCell)
-    })
-    mainTable.appendChild(tableRow)
-  })
+  gameState.forEach(row => renderRow(mainTable, row))
   gameRoot.appendChild(mainTable)
+}
+
+function renderRow(mainTable, row) {
+  const tableRow = document.createElement('tr')
+  row.forEach(cell => renderCells(tableRow, cell))
+  mainTable.appendChild(tableRow)
+}
+
+function renderCells(tableRow, cell) {
+  const tableCell = document.createElement('td')
+  tableCell.innerHTML = cell
+  tableRow.appendChild(tableCell)
 }
